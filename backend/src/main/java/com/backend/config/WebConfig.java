13ce3c6217ba24bcc1fa0c7bd56236afc473d399
msikorsk <01178592@pw.edu.pl>
2024-09-09 22:25:38 +0200

    Fixed package structure

--- backend/src/main/java/com/backend/config/WebConfig.java
+++ backend/src/main/java/com/backend/config/WebConfig.java
@@ -1,6 +1,5 @@
-package com.example.backend.config;
+package com.backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
